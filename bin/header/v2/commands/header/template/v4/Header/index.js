278a8e3bc79d7b0c6d8d2ed68faa9d3a36338598
keshavsoft <nknnkumar@yahoo.com>
2026-06-24 14:21:26 +0530

    pub

---
+++ bin/header/v2/commands/header/template/v4/Header/index.js
@@ -0,0 +1,5 @@
+// import "./input.js";
+import "./Anchor/index.js";
+import "./Icon/index.js";
+
+export default {};
--- bin/header/v2/commands/header/template/v4/index.js
+++ bin/header/v2/commands/header/template/v4/index.js
@@ -1,9 +1,10 @@
-// import "./input.js";
+import "./Header/index.js";
+
 import "./dataListInput/index.js";
 import "./inputDefault/index.js";
 import "./inputForVertical/index.js";
 import "./inputForTableRow/index.js";
 
-console.log("KSTableFoot component v2 loaded to DOM");
+console.log("KSTableFoot component v4 loaded to DOM");
 
 export default {};
