9c0054696086c6fe37477cab2d889e97d27d62bf
keshavsoft <nknnkumar@yahoo.com>
2026-06-24 14:41:55 +0530

    p 6

--- src/header.js
+++ src/header.js
@@ -1,8 +1,8 @@
 // src/header.js
-import initVerticalComp from "../bin/header/v2/commands/header/template/v4/index.js";
+import initVerticalComp from "../bin/header/v2/commands/header/template/v5/index.js";
 
 (async () => {
-    window.KSTableFootVersion = "v2.4";
+    window.KSTableFootVersion = "v2.5";
 
     window.KSTableFoot = initVerticalComp;
 })();
--- src/version.js
+++ src/version.js
@@ -1,2 +1,2 @@
 // src/version.js
-export const templateVersion = "v2.4";
+export const templateVersion = "v2.5";
