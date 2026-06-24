29f8e87dac484dc85584c8656654e69b633a54c9
keshavsoft <nknnkumar@yahoo.com>
2026-06-24 22:08:50 +0530

    feat: add KsTableFooterInputCore component with input configuration and rendering logic

--- bin/header/v2/commands/header/template/v6/inputCore/getInputOptions.js
+++ bin/header/v2/commands/header/template/v6/inputCore/getInputOptions.js
@@ -1,16 +1,35 @@
+const defaultOptions = {
+    inPlaceholder: "",
+    inName: "",
+    inClassName: "w-full border rounded px-2 py-1",
+    inShowDataList: undefined,
+    inColumnsConfig: [],
+    inOnChangeFunc: undefined,
+    inOnChangeType: undefined,
+    inOnKeyDown: undefined,
+    inOnKeyDownType: undefined,
+    inRightAlign: undefined,
+    inWidth: undefined,
+    inputClassName: undefined,
+    inDataListSource: "",
+    inDataListFillName: "",
+    inDataStore: undefined,
+    inputClass: "",
+    inType: "text"
+};
+
 const getInputOptions = ({ inElement }) => {
     const localName =
-        inElement.ksName || "";
+        inElement.ksName || defaultOptions.inName;
 
-    console.log("inElement : ", inElement);
+    // console.log("inElement : ", inElement);
 
     return {
-        inPlaceholder: inElement.ksPlaceholder || "",
+        inPlaceholder: inElement.ksPlaceholder || defaultOptions.inPlaceholder,
         inName: localName,
-        inClassName: inElement.ksClassName ||
-            "w-full border rounded px-2 py-1",
+        inClassName: inElement.ksClassName || defaultOptions.inClassName,
         inShowDataList: inElement.ksShowDataList,
-        inColumnsConfig: inElement.ksInColumnsConfig || [],
+        inColumnsConfig: inElement.ksInColumnsConfig || defaultOptions.inColumnsConfig,
         inOnChangeFunc: inElement.ksOnChangeFunc,
         inOnChangeType: inElement.ksOnChangeType,
         inOnKeyDown: inElement.ksOnKeyDown,
@@ -18,12 +37,14 @@ const getInputOptions = ({ inElement }) => {
         inRightAlign: inElement.ksRightAlign,
         inWidth: inElement.ksWidth,
         inputClassName: inElement.ksInputClassName,
-        inDataListSource: inElement.getAttribute("ksDataListSource") || "",
-        inDataListFillName: inElement.getAttribute("ksDataListFillName") || "",
+        inDataListSource: inElement.getAttribute("ksDataListSource") || defaultOptions.inDataListSource,
+        inDataListFillName: inElement.getAttribute("ksDataListFillName") || defaultOptions.inDataListFillName,
         inDataStore: inElement.dataStore,
-        inputClass: inElement.getAttribute("ksInputClass") || "",
-        inType: inElement.getAttribute("ksType") || "text"
+        inputClass: inElement.getAttribute("ksInputClass") || defaultOptions.inputClass,
+        inType: inElement.getAttribute("ksType") || defaultOptions.inType
     };
 };
 
+export { defaultOptions };
 export default getInputOptions;
+
