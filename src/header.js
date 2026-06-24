ea54a7896ea942dc955f7c7ee9da86318f56186e
keshavsoft <nknnkumar@yahoo.com>
2026-06-24 22:44:52 +0530

    feat: initialize KSTableComponents library and add navigation component with SVG assets

--- src/header.js
+++ src/header.js
@@ -1,8 +1,8 @@
 // src/header.js
-import initVerticalComp from "../bin/header/v2/commands/header/template/v5/index.js";
+import initVerticalComp from "../bin/header/v2/commands/header/template/v6/index.js";
 
 (async () => {
-    window.KSTableFootVersion = "v2.5";
+    window.KSTableFootVersion = "v2.6";
 
     window.KSTableFoot = initVerticalComp;
 })();
