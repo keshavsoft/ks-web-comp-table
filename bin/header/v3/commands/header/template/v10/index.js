38cb54d55fcd2804d2573ea275cc0b3f8c258b8a
keshavsoft <nknnkumar@yahoo.com>
2026-07-03 13:04:23 +0530

    new v3

---
+++ bin/header/v3/commands/header/template/v10/index.js
@@ -0,0 +1,11 @@
+import "./Header/index.js";
+
+import "./dataListInput/index.js";
+import "./inputDefault/index.js";
+import "./inputForVertical/index.js";
+import "./inputForTableRow/index.js";
+import "./tableBodyOptionsCell/index.js";
+
+console.log("KS Components v9 loaded to DOM");
+
+export default {};
---
+++ bin/header/v3/commands/header/template/v9/tableBodyOptionsCell/createEditButton.js
@@ -0,0 +1,9 @@
+const createEditButton = ({ item, index, onEditFunc }) => {
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.className = "px-2 py-1 bg-yellow-400 text-white rounded";
+    editBtn.onclick = () => onEditFunc?.({ item, index, presentPk: item?.pk });
+    return editBtn;
+};
+
+export default createEditButton;
